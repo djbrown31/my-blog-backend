39adcd59c02168cfa7ac158bf13f3bb19a765203
Dan Brown <brown.danny.j@gmail.com>
2020-09-22 18:33:01 -0500

    commit build

---
+++ src/build/precache-manifest.c2e6f1d0a7666447679b0718e98ad3ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "490b496952d7ac7df5b99bb0bdf48b38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f898fcee130f2ba69d0",
+    "url": "/static/css/main.da04e392.chunk.css"
+  },
+  {
+    "revision": "51fbfb2949c8bf00e74f",
+    "url": "/static/js/2.4ad77ef8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4ad77ef8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f898fcee130f2ba69d0",
+    "url": "/static/js/main.bf28289b.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
